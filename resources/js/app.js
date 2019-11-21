127d2000e16197abbfe1a0a044748f253bab16b2
[Yousef Alsatouf] <yousef.alsatouf94@gmail.com>
2019-11-21 16:26:45 +0100

    database setting to online

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,7 +14,7 @@ import App from './App.vue';
 Vue.use(VueAxios, axios);
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
-axios.defaults.baseURL = 'http://shrouded-beach-95835.herokuapp.com/api';
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 
 import HomeComponent from './components/HomeComponent.vue';
 import CreateComponent from './components/CreateComponent.vue';
--- resources/js/components/CreateComponent.vue
+++ resources/js/components/CreateComponent.vue
@@ -36,7 +36,7 @@
         },
         methods: {
             addPost(){
-                let uri = 'http://shrouded-beach-95835.herokuapp.com/api/post/create';
+                let uri = 'http://127.0.0.1:8000/api/post/create';
                 this.axios.post(uri, this.post).then((response) => {
                     this.$router.push({name: 'posts'});
                 });
